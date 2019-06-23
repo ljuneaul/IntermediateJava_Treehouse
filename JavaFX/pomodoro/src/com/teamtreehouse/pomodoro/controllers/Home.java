a3c771850729cc2c7533e5dfe68791dc74adc5d3
Juneau <jungwon.limm@gmail.com>
2019-06-23 09:41:01 -0400

    -

---
+++ JavaFX/pomodoro/src/com/teamtreehouse/pomodoro/controllers/Home.java
@@ -0,0 +1,66 @@
+package com.teamtreehouse.pomodoro.controllers;
+
+import com.teamtreehouse.pomodoro.model.Attempt;
+import com.teamtreehouse.pomodoro.model.AttemptKind;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class Home {
+    @FXML
+    private VBox container;
+
+    @FXML
+    private Label title;
+
+    private Attempt mCurrentAttempt;
+    private StringProperty mTimerText;
+
+    public Home() {
+        mTimerText = new SimpleStringProperty();
+        setTimerText(0);
+    }
+
+    public String getTimerText() {
+        return mTimerText.get();
+    }
+
+    public StringProperty timerTextProperty() {
+        return mTimerText;
+    }
+
+    public void setTimerText(String timerText) {
+        mTimerText.set(timerText);
+    }
+
+    public void setTimerText(int remainingSeconds) {
+        int minutes = remainingSeconds / 60;
+        int seconds = remainingSeconds % 60;
+        setTimerText(String.format("%02d:%02d", minutes, seconds));
+    }
+
+    private void prepareAttempt(AttemptKind kind) {
+        clearAttemptStyles();
+        mCurrentAttempt = new Attempt(kind, "");
+        addAttemptStyle(kind);
+        title.setText(kind.getDisplayName());
+        setTimerText(mCurrentAttempt.getRemainingSeconds());
+    }
+
+    private void addAttemptStyle(AttemptKind kind) {
+        container.getStyleClass().add(kind.toString().toLowerCase());
+    }
+
+    private void clearAttemptStyles() {
+        for (AttemptKind kind : AttemptKind.values()) {
+            container.getStyleClass().remove(kind.toString().toLowerCase());
+        }
+    }
+
+    public void DEBUG(ActionEvent actionEvent) {
+        System.out.println("HI MOM");
+    }
+}
